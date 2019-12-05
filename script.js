8994b0043e09b294049c6928e6c1099c2122c395
Takabachi <hujiisistem@gmail.com>
2019-12-05 12:17:12 +0900

    変数とrequestAnimFrameの修正、コメント記述などのコミット

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 window.requestAnimFrame = (function(){
   return (
     window.requestAnimationFrame       ||
-    window.webkitRequestAnimationFrame ||
-    window.mozRequestAnimationFrame    ||
-    window.oRequestAnimationFrame      ||
-    window.msRequestAnimationFrame     ||
     function(callback, time) {
       var time = time ? time: 1000 / 60;
       window.setTimeout(callback, time);
@@ -12,10 +8,12 @@ window.requestAnimFrame = (function(){
   ); 
 })();
 
-let canvas = document.getElementById("sample");
-let ctx = canvas.getContext("2d");
+var canvas = document.getElementById("sample"); //定数
+var ctx = canvas.getContext("2d");  //定数
 
-let Particle = function(scale, color, speed){
+function Particle(scale, color, speed){
+  //このthisは関数呼び出しパターン
+  //this=Particle
   this.scale = scale; // 大きさ
   this.color = color; //色
   this.speed = speed; //速度
@@ -29,25 +27,25 @@ Particle.prototype.draw = function() {
   ctx.fill();
 };
 
-let density = 300;  
-let particles = []; 
-let colors = ['gold', 'crimson', 'deepskyblue', 'lime'];
+var density = 300;  //定数
+var particles = []; //定数
+var colors = ['gold', 'crimson', 'deepskyblue', 'lime'];  //定数
 
-for (let i=0; i<density; i++) {
-  let color = colors[~~(Math.random()*4)];
-  let scale = ~~(Math.random()*(8-3))+3;
+for (var i=0; i<density; i++) {
+  var color = colors[~~(Math.random()*4)];  //定数
+  var scale = ~~(Math.random()*(8-3))+3;    //定数
   particles[i] = new Particle(scale, color, scale/2);
   particles[i].position.x = Math.random()*canvas.width;
   particles[i].position.y = Math.random()*canvas.height;
   particles[i].draw();
 }
 
-let loop = () => {
+function loop(){
   requestAnimFrame(loop);
   
   ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
   
-  for (let i=0; i<density; i++) {
+  for (var i=0; i<density; i++) {
     particles[i].position.x += particles[i].speed;
     particles[i].draw();
 
